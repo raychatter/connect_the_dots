4beff6f2f1ffb747ed82c44e7e658041f67be0eb
raychatter <rachel.e.walker@gmail.com>
2014-02-08 15:47:52 -0800

    changes in the controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to(root_url)
     else
       render 'new'
     end
