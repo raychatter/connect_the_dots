096f1e5b86340598859b3137bf38884b55cfc612
raychatter <rachel.e.walker@gmail.com>
2014-05-24 16:55:09 -0700

    Deleting everything except .gitignore

--- db/migrate/20140209011729_add_up_and_down_vote_to_microposts.rb
+++
@@ -1,6 +0,0 @@
-class AddUpAndDownVoteToMicroposts < ActiveRecord::Migration
-  def change
-    add_column :microposts, :up_vote, :integer
-    add_column :microposts, :down_vote, :integer
-  end
-end
