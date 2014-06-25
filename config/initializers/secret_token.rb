5217489cf18bff5fe9fd5a702ef3e0aa05f22743
raychatter <rachel.e.walker@gmail.com>
2014-06-25 10:31:02 -0700

    Initial framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConnectTheDots::Application.config.secret_key_base = '7340e0534b18bcac228d4eefd4bab582e974ed380cf044af203b807cf4e8c3540735f71cd6ec2e999147b3ab2577bfb229c919e258f678cd49583897f54598ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ConnectTheDots::Application.config.session_store :cookie_store, key: '_connect_the_dots_session'
