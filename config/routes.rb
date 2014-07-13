c0d7040869b9a492bf092e9c31d419a321a7d555
raychatter <rachel.e.walker@gmail.com>
2014-07-12 20:45:47 -0700

    Whoopsy... fixing help to about in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ ConnectTheDots::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'static_pages#home'
+   match '/about', to: 'static_pages#about', via: 'get'
+   match '/assistance', to: 'static_pages#assistance', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
