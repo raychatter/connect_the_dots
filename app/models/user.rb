20ac7e2523bb4e919f09c77b8bf316e943659d44
raychatter <rachel.e.walker@gmail.com>
2014-07-13 23:52:03 -0700

    Adding email validation and posts relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :posts
+	before_save { self.email = email.downcase }
+	validates :username, presence: true, uniqueness: {case_sensative: false}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensative: false}
 end
