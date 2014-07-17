e207f87645610abcdccfb94150d21261b9aa0b30
raychatter <rachel.e.walker@gmail.com>
2014-07-17 10:13:16 -0700

    Showing list of replies on feed page

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,6 @@
 class Reply < ActiveRecord::Base
   belongs_to :micropost
   belongs_to :user
-  default_scope -> { order('created_at DESC') }
+  default_scope -> { order('created_at ASC') }
   validates :content, presence: true
 end
